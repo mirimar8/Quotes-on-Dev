b235193010e6f14b5cab82029308eb3d92d4c35e
Miri <mirimarkovitzbl@gmail.com>
2019-08-20 11:14:12 -0700

    fix the search functionality.

--- themes/Quotes-on-Dev-theme/template-parts/content-search.php
+++ themes/Quotes-on-Dev-theme/template-parts/content-search.php
@@ -8,23 +8,27 @@
 ?>
 
 			<?php
-				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-				$args = array(
-				'posts_per_page'   => 10,
-				'post_type' => 'post',
-				'paged' => $paged
-				);
+				// $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				// $args = array(
+				// 'posts_per_page'   => 10,
+				// 'post_type' => 'post',
+				// 'paged' => $paged
+				// );
 			?> 
 
-			<?php $query = new WP_Query( $args ); ?>
-			<?php if ( $query->have_posts() ) : ?>
-				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+			 <?php //$query = new WP_Query( $args ); ?>
+			<?php //if ( have_posts() ) : ?>
+				<?php //while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<header class="entry-header">
 					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">',
 					 esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+					<?php if ( 'post' === get_post_type() ) : ?>
+						
+					<?php endif; ?>
 
 				</header><!-- .entry-header -->
 
@@ -34,14 +38,14 @@
 
 				
 				
-				<?php endwhile; ?>
+				<?php //endwhile; ?>
 					
 
-				<?php wp_reset_postdata(); ?>
+				<?php //wp_reset_postdata(); ?>
 				
-				<?php else : ?>
-					<h2>Nothing found!</h2>
-				<?php endif; ?>
+				<?php //else : ?>
+					<!-- <h2>Nothing found!</h2> -->
+				<?php //endif; ?>
 
 				</article><!-- #post-## -->
 
